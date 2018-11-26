fed913d057e9039d16eafd92d75df1596cc73c70
Devin <gendron.devin@gmail.com>
2018-11-25 23:11:55 -0500

    Update dbcon.js

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_gendrond',
-    password        : '1201',
+    password        : '####',
     database        : 'cs340_gendrond'
 });
 module.exports.pool = pool;
